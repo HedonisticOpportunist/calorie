495ea627ffc82af7e59a6c4ef67768f9b60fa111
HedonisticOpportunist <fujitahibaraki@gmail.com>
2020-04-17 23:30:10 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function showCalories() {
+  var calories = document.getElementById("calories").value
+  caloriesToSave = calories / 7
+  alert("You need to save: " + caloriesToSave + " calories.")
+}
